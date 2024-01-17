b48d41de3cf7b7090a68a8f1ebfcf01aa84b6135
Mainul Islam <mainulislam6168@gmail.com>
2024-01-17 12:13:42 +0600

    crud operation done

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const port = process.env.PORT || 5000;
 
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("Health is wealth")
+  res.send("Health is wealth")
 })
 
 
@@ -30,12 +30,58 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const serviceCollection = client.db("mentalHealth").collection("service");
+
+    // 1. insert a service to db
+    app.post("/service", async (req, res) => {
+      const service = req.body;
+      console.log(service);
+      const result = await serviceCollection.insertOne(service);
+      res.send(result);
+    })
+
+    // 2.get all service data
+    app.get("/all-services", async(req, res) => {
+      const services = serviceCollection.find();
+      const result = await services.toArray();
+      res.send(result);
+    })
+
+    // 2.1 get a specific data
+    app.get("/services/:id", async(req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const result = await serviceCollection.findOne(filter);
+      res.send(result)
+    })
+    // 3. update a service
+    app.patch("/services/:id", async(req, res) => {
+      const id = req.params.id;
+      const updatedService = req.body;
+      const filter = {_id: new ObjectId(id)};
+      const updatedDoc = {
+        $set: {
+          ...updatedService
+        }
+      }
+      const result = await serviceCollection.updateOne(filter, updatedDoc);
+      res.send(result)
+
+    })
+    // 4. delete
+    app.delete("/services/:id", async(req, res) => {
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)};
+      const result = await serviceCollection.deleteOne(filter);
+      res.send(result)
+
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-   
+
   }
 }
 run().catch(console.log);
@@ -43,5 +89,5 @@ run().catch(console.log);
 
 
 app.listen(port, () => {
-    console.log('port is running on', +  port)
+  console.log(`port is running on ${port}`)
 })
